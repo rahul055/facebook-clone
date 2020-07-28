2680f48d8f775f8a643e535aaf3aaccea9496248
rahul055 <rrjoshi055@gmail.com>
2020-07-29 00:24:23 +0530

    setting up components for api calls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './App.css';
+import { userRef } from './firebase/firebase'
 
 function App() {
+  useEffect(() => {
+
+    userRef.push({
+      email: 'rj.test@gmail.com',
+      password: '12345'
+    })
+
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA93X05qngO_0WVMPV705FaAX7tGTLtVtg",
+    authDomain: "facebook-clone-rahulj.firebaseapp.com",
+    databaseURL: "https://facebook-clone-rahulj.firebaseio.com",
+    projectId: "facebook-clone-rahulj",
+    storageBucket: "facebook-clone-rahulj.appspot.com",
+    messagingSenderId: "682746244855",
+    appId: "1:682746244855:web:e9eef7edac496049abadf2",
+    measurementId: "G-JFC9HHHQEQ"
+};
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const userRef = firebaseApp.database().ref("users");
